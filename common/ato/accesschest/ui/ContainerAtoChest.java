860260e031025e4da158f40cdb83f64d759b42dc
AtoCrafter <ato.crafter@gmail.com>
2013-03-05 09:59:00 +0900

    Container#transferStackInSlot を実装

--- common/ato/accesschest/ui/ContainerAtoChest.java
+++ common/ato/accesschest/ui/ContainerAtoChest.java
@@ -1,10 +1,11 @@
 package ato.accesschest.ui;
 
-import ato.accesschest.repository.Repository;
+import ato.accesschest.game.ItemAccessChest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 /**
  * この MOD で追加されるチェストの GUI でプレイヤーインベントリとチェスト内を組み合わせるコンテナ
@@ -15,7 +16,7 @@ public class ContainerAtoChest extends Container {
     private IInventory playerInventory;
 
     /**
-     * @param chestInventory 対象とするチェストのインベントリ
+     * @param chestInventory  対象とするチェストのインベントリ
      * @param playerInventory GUI を開いているプレイヤーのインベントリ
      */
     public ContainerAtoChest(IInventory chestInventory, IInventory playerInventory) {
@@ -31,29 +32,29 @@ public class ContainerAtoChest extends Container {
         inventorySlots.clear();
         // chest inventory
         int max = chestInventory.getSizeInventory();
-        for ( int k=0; k<8; k++ ) {
-            for ( int l=0; l<12; l++ ) {
+        for (int k = 0; k < 8; k++) {
+            for (int l = 0; l < 12; l++) {
 //                int stackIndex = getCurrentScroll()*12 + k*12+l;
-                int index = getDisplayBaseIndex() + k*12 + l;
-                if ( index < max ) {
-                    addSlotToContainer(new Slot(chestInventory, index, l*18+12, k*18+17));
+                int index = getDisplayBaseIndex() + k * 12 + l;
+                if (index < max) {
+                    addSlotToContainer(new Slot(chestInventory, index, l * 18 + 12, k * 18 + 17));
                 }
             }
         }
         // player inventory
-        for ( int k=0; k<3; k++ ) {
-            for ( int l=0; l<9; l++ ) {
-                addSlotToContainer(new Slot(playerInventory, k*9+l+9, l*18+12, k*18+165));
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 9; l++) {
+                addSlotToContainer(new Slot(playerInventory, k * 9 + l + 9, l * 18 + 12, k * 18 + 165));
             }
         }
         // player itemslot
-        for  ( int l=0; l<9; l++ ) {
-            addSlotToContainer(new Slot(playerInventory, l, l*18+12, 223));
+        for (int l = 0; l < 9; l++) {
+            addSlotToContainer(new Slot(playerInventory, l, l * 18 + 12, 223));
         }
         // inventoryItemStacks size
-        int limit = 12*8 + 9*4;
-        while ( inventoryItemStacks.size() > limit ) {
-            inventoryItemStacks.remove(inventoryItemStacks.size()-1);
+        int limit = 12 * 8 + 9 * 4;
+        while (inventoryItemStacks.size() > limit) {
+            inventoryItemStacks.remove(inventoryItemStacks.size() - 1);
         }
 //        updateSlotSize();
     }
@@ -68,5 +69,84 @@ public class ContainerAtoChest extends Container {
     @Override
     public boolean canInteractWith(EntityPlayer player) {
         return true;
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+        ItemStack stackBackup = null;
+        Slot slot = (Slot) inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack()) {
+            ItemStack slotStack = slot.getStack();
+            stackBackup = slotStack.copy();
+
+            int border = inventorySlots.size() - 36;
+            if (index < border) {
+                if (!mergeItemStack(slotStack, border, inventorySlots.size(), false)) {
+                    return null;
+                }
+            } else if (slotStack.getItem() instanceof ItemAccessChest ||  // forbit to put AccessChest away in AccessChest automatically
+                    !customMergeItemStack(slotStack)) {
+                return null;
+            }
+            if (slotStack.stackSize == 0) {
+                slot.putStack(null);
+            } else {
+                slot.onSlotChanged();
+            }
+        }
+
+        return stackBackup;
+    }
+
+    /**
+     * プレイヤーのインベントリからこの MOD のチェストのインベントリに Shift クリックなどで移動させるとき
+     * 画面に表示されていないスロットも含めて、自動的に移動させる
+     *
+     * @param stack 移動させたいアイテムスタック
+     * @return 成否
+     * @see Container#mergeItemStack(net.minecraft.item.ItemStack, int, int, boolean)
+     */
+    public boolean customMergeItemStack(ItemStack stack) {
+        boolean isSuccess = false;
+        int max = chestInventory.getSizeInventory();
+        ItemStack chestStack;
+
+        // 同じ種類のスタック可能なスロットを探して、可能な数だけ結合させる
+        if (stack.isStackable()) {
+            for (int i = 0; i < max; ++i) {
+                chestStack = chestInventory.getStackInSlot(i);
+                if (chestStack != null && chestStack.itemID == stack.itemID &&
+                        (!stack.getHasSubtypes() || stack.getItemDamage() == chestStack.getItemDamage()) &&
+                        ItemStack.areItemStackTagsEqual(stack, chestStack)) {
+                    int sum = chestStack.stackSize + stack.stackSize;
+                    if (sum <= chestStack.getMaxStackSize()) {
+                        stack.stackSize = 0;
+                        chestStack.stackSize = sum;
+                        isSuccess = true;
+                    } else if (chestStack.stackSize < stack.getMaxStackSize()) {
+                        stack.stackSize -= stack.getMaxStackSize() - chestStack.stackSize;
+                        chestStack.stackSize = chestStack.getMaxStackSize();
+                        isSuccess = true;
+                    }
+                }
+                if (stack.stackSize == 0) break;
+            }
+        }
+
+        // 空きスロットを探して、可能な数だけ挿入する
+        if (stack.stackSize > 0) {
+            for (int i = 0; i < max; ++i) {
+                chestStack = chestInventory.getStackInSlot(i);
+                if (chestStack == null) {
+                    chestInventory.setInventorySlotContents(i, stack.copy());
+                    stack.stackSize = 0;
+                    isSuccess = true;
+                    break;
+                }
+            }
+        }
+
+        return isSuccess;
     }
 }
