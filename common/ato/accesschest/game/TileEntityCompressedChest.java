a118fb68cf9b34d9b11cf1554c8d08c7d5d01b1c
AtoCrafter <ato.crafter@gmail.com>
2013-03-15 20:14:47 +0900

    Compressed Chest のデータを保存できるように

--- common/ato/accesschest/game/TileEntityCompressedChest.java
+++ common/ato/accesschest/game/TileEntityCompressedChest.java
@@ -2,11 +2,29 @@ package ato.accesschest.game;
 
 import ato.accesschest.repository.Repository;
 import ato.accesschest.repository.RepositoryCompressedChest;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class TileEntityCompressedChest extends TileEntityAtoChest {
 
+    private int color;
+    private int grade;
+
     @Override
     protected Repository createRepository(int color, int grade) {
+        this.color = color;
+        this.grade = grade;
         return new RepositoryCompressedChest(grade);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        ((RepositoryCompressedChest) repo).readFromNBT(nbt);
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+        ((RepositoryCompressedChest) repo).writeToNBT(nbt);
     }
 }
--- common/ato/accesschest/repository/RepositoryCompressedChest.java
+++ common/ato/accesschest/repository/RepositoryCompressedChest.java
@@ -1,16 +1,26 @@
 package ato.accesschest.repository;
 
+import net.minecraft.nbt.NBTTagCompound;
+
 /**
  * Compressed Chest の実体
  */
 public class RepositoryCompressedChest extends Repository {
 
     public RepositoryCompressedChest(int grade) {
-        super(new DataManagerArray());
+        super(new DataManagerNBT());
     }
 
     @Override
     public String getInvName() {
         return "Compressed Chest";
+    }
+
+    public void readFromNBT(NBTTagCompound nbt) {
+        ((DataManagerNBT) data).readFromNBT(nbt);
+    }
+
+    public void writeToNBT(NBTTagCompound nbt) {
+        ((DataManagerNBT) data).writeToNBT(nbt);
     }
 }
