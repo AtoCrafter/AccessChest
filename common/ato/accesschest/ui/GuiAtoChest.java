33b48127f1e3333534fc9a236a77123b2e80bcb3
AtoCrafter <ato.crafter@gmail.com>
2013-07-25 23:40:56 +0900

    テクスチャ、言語ファイルの場所を assets 内に移動

--- common/ato/accesschest/ui/GuiAtoChest.java
+++ common/ato/accesschest/ui/GuiAtoChest.java
@@ -115,7 +115,7 @@ public class GuiAtoChest extends GuiContainer {
     @Override
     protected void drawGuiContainerBackgroundLayer(float f, int i1, int i2) {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        mc.func_110434_K().func_110577_a(new ResourceLocation("accesschest", "gui/atochest.png"));
+        mc.func_110434_K().func_110577_a(new ResourceLocation("accesschest", "/gui/atochest.png"));
         int j = (width - xSize) / 2;
         int k = (height - ySize) / 2;
         drawTexturedModalRect(j, k + 9, 0, 0, xSize, ySize - 18);
