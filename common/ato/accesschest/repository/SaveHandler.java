278dc81ccb0aab31bc0813b36402dea7bf9fb74a
AtoCrafter <ato.crafter@gmail.com>
2013-11-04 07:39:40 +0900

    OldSavedataConverter を削除

--- common/ato/accesschest/repository/SaveHandler.java
+++ common/ato/accesschest/repository/SaveHandler.java
@@ -8,25 +8,18 @@ import net.minecraftforge.event.world.WorldEvent;
  */
 public class SaveHandler {
 
-    private static final OldSavedataConverter converter = new OldSavedataConverter();
-
-    /**
-     * ワールド読み込み時に Access Chest のリポジトリ群をリロードする
-     */
-    @ForgeSubscribe
-    public void reloadPool(WorldEvent.Load event) {
-        for (int i = 0; i < 16; ++i) {
-            String name = "AccessChest" + i;
-            if (event.world.loadItemData(DataManagerWorldSaveData.class, name) == null) {
-                if (converter.doesOldSavedataExists(i)) {
-                    DataManagerWorldSaveData manager = new DataManagerWorldSaveData(name);
-                    manager.readFromNBT(converter.getOldNBT(i));
-                    event.world.setItemData(name, manager);
-                } else {
-                    // セーブデータが存在しない場合は新規作成
-                    event.world.setItemData(name, new DataManagerWorldSaveData(name));
-                }
-            }
-        }
-    }
+	/**
+	 * ワールド読み込み時に Access Chest のリポジトリ群をリロードする
+	 */
+	@ForgeSubscribe
+	public void reloadPool(WorldEvent.Load event) {
+		for (int i = 0; i < 16; ++i) {
+			String name = "AccessChest" + i;
+			if (event.world.loadItemData(DataManagerWorldSaveData.class, name) == null) {
+				// セーブデータが存在しない場合は新規作成
+				event.world.setItemData(name,
+						new DataManagerWorldSaveData(name));
+			}
+		}
+	}
 }
